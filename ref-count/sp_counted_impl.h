8db651ced626cf6d5a69fbe052ab579458b86b35
flying-pig <chenhuaying@sina.cn>
2016-05-20 23:28:23 +0800

    format code

--- ref-count/sp_counted_impl.h
+++ ref-count/sp_counted_impl.h
@@ -11,19 +11,19 @@ typedef struct sp_counted_impl_p_s {
 
 static inline void sp_counted_impl_despose(sp_counted_base *pi)
 {
-  if (pi != NULL) free(((sp_counted_impl_p *)pi)->px_);
+	if (pi != NULL) free(((sp_counted_impl_p *)pi)->px_);
 }
 
 static inline void *sp_counted_impl_get_deleter(sp_counted_base *pi)
 {
-  (void)pi;
-  return NULL;
+	(void)pi;
+	return NULL;
 }
 
 static inline void *sp_counted_impl_get_untyped_deleter(sp_counted_base *pi)
 {
-  (void)pi;
-  return NULL;
+	(void)pi;
+	return NULL;
 }
 
 static inline sp_counted_base *sp_counted_impl_p_new(void *px)
@@ -31,9 +31,9 @@ static inline sp_counted_base *sp_counted_impl_p_new(void *px)
 	sp_counted_impl_p *p = (sp_counted_impl_p *)calloc(1, sizeof(sp_counted_impl_p));
 	sp_counted_init((sp_counted_base *)p);
 	p->px_ = px;
-  p->base.dispose = sp_counted_impl_despose;
-  p->base.get_deleter = sp_counted_impl_get_deleter;
-  p->base.get_untyped_deleter = sp_counted_impl_get_untyped_deleter;
+	p->base.dispose = sp_counted_impl_despose;
+	p->base.get_deleter = sp_counted_impl_get_deleter;
+	p->base.get_untyped_deleter = sp_counted_impl_get_untyped_deleter;
   return (sp_counted_base *)p;
 }
 
