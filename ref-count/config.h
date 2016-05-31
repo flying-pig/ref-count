63d2033acd0d7016b35ce306e61781503a9fe7ce
chenhuaying <chenhuaying@ChenHuaying.local>
2016-05-31 14:59:13 +0800

    modify BOOST macro to REF_COUNT

--- ref-count/config.h
+++ ref-count/config.h
@@ -2,18 +2,18 @@
 #define __REF_COUNT_CONFIG_H_
 
 #include <config/select_compiler_config.h>
-#ifdef BOOST_COMPILER_CONFIG
-#  include BOOST_COMPILER_CONFIG
+#ifdef REF_COUNT_COMPILER_CONFIG
+#  include REF_COUNT_COMPILER_CONFIG
 #endif
 
 #include <config/select_stdlib_config.h>
-#ifdef BOOST_STDLIB_CONFIG
-#include BOOST_STDLIB_CONFIG
+#ifdef REF_COUNT_STDLIB_CONFIG
+#include REF_COUNT_STDLIB_CONFIG
 #endif
 
 #include <config/select_platform_config.h>
-#ifdef BOOST_PLATFORM_CONFIG
-#include BOOST_PLATFORM_CONFIG
+#ifdef REF_COUNT_PLATFORM_CONFIG
+#include REF_COUNT_PLATFORM_CONFIG
 #endif
 
 #ifdef REF_COUNT_HAS_PRAGMA_ONCE
--- ref-count/config/select_compiler_config.h
+++ ref-count/config/select_compiler_config.h
@@ -1,13 +1,13 @@
 #if defined __clang__ && !defined(__CUDACC__) && !defined(__ibmxl__)
 // when using clang and cuda at same time, you want to appear as gcc
 //  Clang C++ emulates GCC, so it has to appear early.
-#   define BOOST_COMPILER_CONFIG "config/compiler/clang.h"
+#   define REF_COUNT_COMPILER_CONFIG "config/compiler/clang.h"
 
 # elif defined(__GNUC__) && !defined(__ibmxl__)
 //  GNU C++:
-#   define BOOST_COMPILER_CONFIG "config/compiler/gcc.h"
+#   define REF_COUNT_COMPILER_CONFIG "config/compiler/gcc.h"
 
-#elif defined (BOOST_ASSERT_CONFIG)
+#elif defined (REF_COUNT_ASSERT_CONFIG)
 // this must come last - generate an error if we don't
 // recognise the compiler:
 #  error "Unknown compiler"
